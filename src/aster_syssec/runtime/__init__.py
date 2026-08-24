c11f7530d4f6c6a7094041e19fd59e95592d9970
Ruowen Qin <chinqrw@gmail.com>
2026-08-25 01:20:44 +0800

    feat: run Asterinas runtime cases in QEMU
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- src/aster_syssec/runtime/__init__.py
+++ src/aster_syssec/runtime/__init__.py
@@ -1,5 +1,6 @@
 """Runtime verification support."""
 
+from .asterinas import AsterinasQemuAdapter
 from .protocol import GuestProtocolError, parse_guest_result
 
-__all__ = ["GuestProtocolError", "parse_guest_result"]
+__all__ = ["AsterinasQemuAdapter", "GuestProtocolError", "parse_guest_result"]
